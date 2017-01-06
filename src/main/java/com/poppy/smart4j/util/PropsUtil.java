162b0abf14aeca5e6df74747f5f9b5a73d9b365b
chenb-c <lspopi@163.com>
2017-01-06 13:19:15 +0800

    添加默认界面已经属性文件工具类

---
+++ src/main/java/com/poppy/smart4j/util/PropsUtil.java
@@ -0,0 +1,68 @@
+package com.poppy.smart4j.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * 属性文件工具类
+ * @author chenb-c
+ * @date 2017/1/6
+ */
+public class PropsUtil {
+    private static  final Logger LOGGER= LoggerFactory.getLogger(PropsUtil.class);
+
+    public static Properties loadProps(String fileName){
+        Properties properties = null;
+        InputStream in = null;
+        try {
+            in = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+            if(in==null){
+                throw  new FileNotFoundException(fileName+" file is not found");
+            }
+            properties = new Properties();
+            properties.load(in);
+        }catch (IOException e){
+            LOGGER.error("load properties file failure",e);
+        }finally {
+            if(in!=null){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    LOGGER.error("close input stream failure",e);
+                }
+            }
+        }
+        return properties;
+    }
+
+
+    /**
+     * 获取字符串属性，默认值为空
+     * @param props
+     * @param key
+     * @return
+     */
+    public static  String getString(Properties props, String key) {
+        return getString(props, key, "");
+    }
+
+    /**
+     * 获取字符串属性，可指定默认值
+     * @param props
+     * @param key
+     * @param defaultValue
+     * @return
+     */
+    public static  String getString(Properties props,String key,String defaultValue){
+        String value = defaultValue;
+        if(props.containsKey(key)){
+            value = props.getProperty(key);
+        }
+        return value;
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/customer_show.jsp
@@ -0,0 +1,10 @@
+<%@ page pageEncoding="UTF-8" contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1></h1>
+<% // TODO %>
+</body>
+</html>
